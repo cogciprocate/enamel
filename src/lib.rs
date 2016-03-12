3a1f954137daf83e8a5d3f5fe31ea5ebe97bb908
Nick Sanders <cogciprocate@gmail.com>
2016-03-12 15:08:48 -0800

    Remove window from project and return event results.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#![allow(dead_code)]
+
+#[macro_use] extern crate glium;
+extern crate glium_text;
+
+mod ui;
+// // [TEMP]:
+// mod window;
+
+pub mod util;
+
+// // [TEMP]:
+// pub use self::window::Window;
+
+pub use self::ui::{Button, HexButton, TextBox, MouseState, KeyboardState, Element, ElementBorder, 
+	ElementKind, ElementText, Pane, Shape2d, Vertex, CustomEventResult, MouseInputHandler, 
+	KeyboardInputHandler, TextAlign, EventResult, HandlerOption};
---
+++ src/ui/controls/mod.rs
@@ -0,0 +1,7 @@
+mod button;
+mod text_box;
+// mod hex_grid;
+// mod status_text;
+
+pub use self::button::{Button, HexButton};
+pub use self::text_box::TextBox;
